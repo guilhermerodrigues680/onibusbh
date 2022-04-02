51f383d7fb6ce8668d8ed869986174bca7e204e7
guilhermerodrigues680 <guilhermerodrigues680@gmail.com>
2022-04-02 18:15:34 -0300

    removido fallback api pois não funcionou como esperado

--- api/v1/404.js
+++ api/v1/404.js
@@ -1,5 +1,8 @@
 import { getErrResObj } from "./_utils";
 
+// Não funcionou como esperado
+// https://vercel.com/guides/custom-404-page
+
 export default async function handler(request, response) {
   response
     .status(404)
